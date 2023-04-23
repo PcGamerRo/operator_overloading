0d36110d787bea496146b6387a69ff143877d47b
Robert Pietroiu <96921061+PcGamerRo@users.noreply.github.com>
2023-04-23 18:10:00 +0300

    Add project files.

---
+++ operator overloading/Entity.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operator_overloading
+{
+    public class Entity
+    {
+        private int id;
+        private int[] values;
+        public int Id { get => id; set => id = value; }
+        public int[] Values { get => values; set => values = value; }
+
+        public Entity()
+        {
+        }
+
+        public Entity(int id, int[] values)
+        {
+            this.id = id;
+            this.values = values;
+        }
+
+        public static Entity operator+(Entity obj, int val)
+        {
+            obj.id += val;
+            return obj;
+        }
+
+        public static int operator+(int val, Entity obj)
+        {
+            val += obj.id;
+            return val;
+        }
+
+        public static Entity operator ++(Entity obj)
+        {
+            obj.id++;
+            return obj;
+        }
+
+        public int this[int index]
+        {
+            get { return values[index]; }
+            set { values[index] = value; }
+        }
+
+        public static bool operator <(Entity a, Entity b)
+        {
+            if (a.id < b.id)
+                return true;
+            else return false;
+        }
+        public static bool operator >(Entity a, Entity b)
+        {
+            if (a.id > b.id)
+                return true;
+            else return false;
+        }
+    }
+}
---
+++ operator overloading/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operator_overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Entity entity_one = new Entity(1, new int[] { 1, 2 });
+            Entity entity_two = new Entity(2, new int[] { 3, 4 });
+
+            int x = 0;
+            x = x + entity_one;
+            entity_one += 1;
+            entity_one++;
+
+            entity_one[0] = -1;
+            Console.WriteLine(entity_one[0]);
+
+            Console.WriteLine(entity_one.Id + " " + entity_two.Id);
+            Console.WriteLine(entity_one > entity_two);
+        }
+    }
+}
